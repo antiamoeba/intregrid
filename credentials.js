585a4faa7ada013b383c82d03855a68e8c101722
Dorian Chan <dorianchan8x8@gmail.com>
2017-02-07 20:26:10 -0800

    This shouldn't be here

--- credentials.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	tw_client_id:"PiETE8D4F8fdcdAQ2YcSXqgK9",
-	tw_client_secret:"GNzqljmDLiRX8pnel4mHWjtilKIeLufEq9uUSRi85wmxpXYZ0A",
-	google_client_id:"245180641675-eh3re006c7gkhool69p9o1oskj9ij4o4.apps.googleusercontent.com",
-	google_client_secret:"-EHzHggd0tXUUtNZyzmol8Oh"
-}
